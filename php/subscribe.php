bae7bbdf24ac062c0a063028f71c172df013ac11
petun <petun911@gmail.com>
2017-03-16 14:24:00 +0300

    WEB-1952

--- php/subscribe.php
+++ php/subscribe.php
@@ -39,7 +39,7 @@ if ($_POST) {
             ));
             //var_dump($result);
             $array['valid'] = 1;
-            $array['message'] = 'Success! You are successfully subscribed!';
+            $array['message'] = 'Thank you! <br /> We\'ll reach you with our release news as soon as possible!';
 
             $array = array_merge($array, $result);
 
