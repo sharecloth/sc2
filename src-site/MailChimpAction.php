d790c0b8c7d9d47f0e21ef413a18b7a53a52c826
petun <petun911@gmail.com>
2018-03-22 16:51:11 +0300

    add mail chimp

---
+++ src-site/MailChimpAction.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Site;
+
+
+use Petun\Forms\Actions\BaseAction;
+use ZfrMailChimp\Client\MailChimpClient;
+
+class MailChimpAction extends BaseAction
+{
+
+    public $apiKey;
+
+    public $listId;
+
+    function run()
+    {
+        $client = new MailChimpClient($this->apiKey);
+
+        try {
+            $result = $client->subscribe(array(
+                'id' => $this->listId,
+                'email' => array(
+                    'email' => $this->_form->fieldValue('email'),
+                ),
+                'double_optin' => false,
+                'update_existing' => true,
+                'replace_interests' => false,
+                'merge_vars' => [
+
+                ],
+            ));
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+}
